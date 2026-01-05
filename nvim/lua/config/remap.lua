5d8aa91b8be4d681442747c46fb5566c2977eaa7
natlus <jesper.sultan@gmail.com>
2026-01-05 10:53:20 +0100

    nvim: new custom config

---
+++ nvim/lua/config/remap.lua
@@ -0,0 +1,18 @@
+local map = vim.keymap.set
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+map("n", "<leader>sv", vim.cmd.Ex)
+map("n", "<leader>w", ":write<CR>", { desc = "[W]rite" })
+map({ "n", "v" }, "<leader>o", ":update<CR> :source<CR>", { desc = "source current" })
+
+-- paste/del selection without saving the selection to a register
+map("x", "<leader>p", [["_dP]])
+map({ "n", "v" }, "<leader>d", [["_d]])
+
+-- clear search highlight on esc
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+map("n", "do", "<cmd>:Gitsigns preview_hunk_inline<CR>", { desc = "Preview diff hunk" })
+map("n", "dp", "<cmd>:Gitsigns reset_hunk<CR>", { desc = "Reset diff hunk" })
---
+++ nvim/lua/plugins/plugins.lua
@@ -0,0 +1,221 @@
+return {
+	{
+		"datsfilipe/vesper.nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		config = function()
+			---@diagnostic disable-next-line: missing-fields
+			local colors = require("vesper.colors")
+			local mix = require("vesper.utils").mix
+
+			require("vesper").setup({
+				transparent = true,
+
+				overrides = {
+					CursorLine = { bg = mix(colors.bgOption, "#000000", math.abs(0.7)) },
+					Visual = { bg = mix(colors.orange, "#000000", math.abs(0.25)) },
+					GitSignsAdd = { fg = colors.greenLight },
+					GitSignsDelete = { fg = "#ff8080" },
+					GitSignsChange = { fg = colors.orange },
+				},
+
+				palette_overrides = {
+					orange = "#ffc799",
+					orangeLight = "#ffc799",
+					green = "#99ffe4",
+					greenLight = "#99ffe4",
+					fg = "white",
+				},
+			})
+			vim.cmd.colorscheme("vesper")
+		end,
+	},
+
+	"NMAC427/guess-indent.nvim",
+	"justinmk/vim-sneak",
+
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "▌" },
+				change = { text = "▌" },
+				delete = { text = "󰍟" },
+				topdelete = { text = "┃" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged_enable = false,
+			current_line_blame = true,
+			current_line_blame_opts = {
+				delay = 0,
+			},
+			current_line_blame_formatter_nc = "",
+			current_line_blame_formatter = "<author>, <author_time:%R>",
+		},
+	},
+
+	{
+		"rmagatti/auto-session",
+		lazy = false,
+		opts = {
+			suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+		},
+	},
+
+	{
+		"nvim-telescope/telescope.nvim",
+		event = "VimEnter",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+			{ "nvim-telescope/telescope-ui-select.nvim" },
+
+			-- Useful for getting pretty icons, but requires a Nerd Font.
+			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+		},
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(),
+					},
+				},
+			})
+
+			-- Enable Telescope extensions if they are installed
+			pcall(require("telescope").load_extension, "fzf")
+			pcall(require("telescope").load_extension, "ui-select")
+
+			-- See `:help telescope.builtin`
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+			-- vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+			-- Slightly advanced example of overriding default behavior and theme
+			vim.keymap.set("n", "<leader>/", function()
+				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, { desc = "[/] Fuzzily search in current buffer" })
+
+			-- It's also possible to pass additional configuration options.
+			--  See `:help telescope.builtin.live_grep()` for information about particular keys
+			vim.keymap.set("n", "<leader>s/", function()
+				builtin.live_grep({
+					grep_open_files = true,
+					prompt_title = "Live Grep in Open Files",
+				})
+			end, { desc = "[S]earch [/] in Open Files" })
+
+			-- Shortcut for searching your Neovim configuration files
+			vim.keymap.set("n", "<leader>sn", function()
+				builtin.find_files({ cwd = vim.fn.stdpath("config") })
+			end, { desc = "[S]earch [N]eovim files" })
+		end,
+	},
+
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_format = "fallback" })
+				end,
+				mode = "",
+				desc = "[F]ormat buffer",
+			},
+		},
+		opts = {
+			notify_on_error = false,
+			format_on_save = function(bufnr)
+				local disable_filetypes = { c = true, cpp = true }
+				if disable_filetypes[vim.bo[bufnr].filetype] then
+					return nil
+				else
+					return {
+						timeout_ms = 500,
+						lsp_format = "fallback",
+					}
+				end
+			end,
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- You can use 'stop_after_first' to run the first available formatter from the list
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+			},
+		},
+	},
+
+	{
+		"echasnovski/mini.nvim",
+		config = function()
+			require("mini.ai").setup({ n_lines = 500 })
+			require("mini.surround").setup({
+				mappings = {
+					add = "sa", -- Add surrounding in Normal and Visual modes
+					delete = "ds", -- Delete surrounding
+					find = "fs", -- Find surrounding (to the right)
+					replace = "rs", -- Replace surrounding
+				},
+			})
+			require("mini.move").setup({
+				mappings = {
+					down = "J",
+					up = "K",
+				},
+			})
+			require("mini.pairs").setup()
+
+			local statusline = require("mini.statusline")
+			statusline.setup({ use_icons = true })
+
+			---@diagnostic disable-next-line: duplicate-set-field
+			statusline.section_location = function()
+				return "%2l:%-2v"
+			end
+		end,
+	},
+
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			preset = "helix",
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+}
