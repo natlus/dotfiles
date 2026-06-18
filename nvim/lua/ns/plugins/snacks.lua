b2eb591e96e69b54a0ddb9d63f3679324c4ab668
natlus <jesper.sultan@gmail.com>
2026-06-18 08:47:14 +0200

    nvim: use snacks.picker instead of telescope

--- nvim/lua/ns/plugins/snacks.lua
+++ nvim/lua/ns/plugins/snacks.lua
@@ -30,6 +30,44 @@ return {
 					},
 				},
 			},
+
+			matcher = {
+				history_bonus = false,
+				frecency = true,
+			},
+			formatters = {
+				file = {
+					filename_first = true,
+				},
+			},
+			-- layout = {
+			-- 	reverse = true,
+			-- 	layout = {
+			-- 		box = "horizontal",
+			-- 		backdrop = false,
+			-- 		width = 0.8,
+			-- 		height = 0.9,
+			-- 		border = "none",
+			-- 		{
+			-- 			box = "vertical",
+			-- 			{ win = "list", title = " Results ", title_pos = "center", border = true },
+			-- 			{
+			-- 				win = "input",
+			-- 				height = 1,
+			-- 				border = true,
+			-- 				title = "{title} {live} {flags}",
+			-- 				title_pos = "center",
+			-- 			},
+			-- 		},
+			-- 		{
+			-- 			win = "preview",
+			-- 			title = "{preview:Preview}",
+			-- 			width = 0.45,
+			-- 			border = true,
+			-- 			title_pos = "center",
+			-- 		},
+			-- 	},
+			-- },
 		},
 		notifier = { enabled = true },
 		quickfile = { enabled = true },
@@ -37,5 +75,71 @@ return {
 		scroll = { enabled = true },
 		statuscolumn = { enabled = true },
 		words = { enabled = true },
+	},
+	keys = {
+		{
+			"<leader>sf",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>f.",
+			function()
+				Snacks.picker.recent()
+			end,
+			desc = "Recent",
+		},
+		{
+			"gr",
+			function()
+				Snacks.picker.lsp_references()
+			end,
+			nowait = true,
+			desc = "References",
+		},
+		{
+			"gd",
+			function()
+				Snacks.picker.lsp_definitions()
+			end,
+			desc = "Goto Definition",
+		},
+		{
+			"gI",
+			function()
+				Snacks.picker.lsp_implementations()
+			end,
+			desc = "Goto Implementation",
+		},
+		{
+			"<leader>ss",
+			function()
+				Snacks.picker.lsp_symbols()
+			end,
+			desc = "LSP Symbols",
+		},
+		{
+			"<leader>sS",
+			function()
+				Snacks.picker.lsp_workspace_symbols()
+			end,
+			desc = "LSP Workspace Symbols",
+		},
+		{
+			"<leader>se",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "File Explorer",
+		},
+		{
+			"<leader>/",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
 	},
 }
