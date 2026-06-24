3d0b19284ea13465ff3b529f7b81ff75dc398147
natlus <jesper.sultan@gmail.com>
2026-06-24 22:55:43 +0200

    nvim: remove opencode action from snacks.picker

--- nvim/lua/ns/plugins/snacks.lua
+++ nvim/lua/ns/plugins/snacks.lua
@@ -37,28 +37,9 @@ return {
 					exclude = { ".next", ".next-dev", "node_modules" },
 				},
 			},
-			actions = {
-				---@param picker snacks.Picker
-				opencode_send = function(picker)
-					local items = vim.tbl_map(function(item) ---@param item snacks.picker.Item
-						return item.file
-								and require("opencode").format({ path = item.file, from = item.pos, to = item.end_pos })
-							or item.text
-					end, picker:selected({ fallback = true }))
-
-					require("opencode").prompt(table.concat(items, ", ") .. " ")
-				end,
-			},
-			win = {
-				input = {
-					keys = {
-						["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
-					},
-				},
-			},
-
 			matcher = {
-				history_bonus = false,
+				sort_empty = true,
+				history_bonus = true,
 				frecency = true,
 			},
 			formatters = {
