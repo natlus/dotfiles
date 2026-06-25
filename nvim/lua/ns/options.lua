78a91b50f01d67d1ecdb3b9a08e6fa2936e31c95
natlus <jesper.sultan@gmail.com>
2026-06-25 13:44:44 +0200

    nvim: support nvim 12

--- nvim/lua/ns/options.lua
+++ nvim/lua/ns/options.lua
@@ -27,6 +27,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking text",
 	group = vim.api.nvim_create_augroup("ns-highlight-yank", { clear = true }),
 	callback = function()
-		vim.hl.hl_op()
+		vim.highlight.on_yank()
 	end,
 })
