91824fa0ac170bfa6887b675fdcaa997232eff40
natlus <jesper.sultan@gmail.com>
2026-07-04 17:28:20 +0200

    nvim: add fullscreen fugitive command and keybind

--- nvim/lua/ns/plugins/fugitive.lua
+++ nvim/lua/ns/plugins/fugitive.lua
@@ -1,3 +1,9 @@
 return {
 	"tpope/vim-fugitive",
+	keys = {
+		{ "<leader>G", "<cmd>tab Git<cr>", desc = "Git status tab" },
+	},
+	config = function()
+		vim.api.nvim_create_user_command("G", "tab Git", {})
+	end,
 }
