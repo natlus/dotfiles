224d9f62609321f1b2b6f35811a15d0121f41731
natlus <jesper.sultan@gmail.com>
2026-07-15 08:22:27 +0200

    nvim: allow deleting items in harpoon

--- nvim/lua/ns/plugins/harpoon.lua
+++ nvim/lua/ns/plugins/harpoon.lua
@@ -28,6 +28,26 @@ return {
 						harpoon_files:select(item.idx)
 					end
 				end,
+				actions = {
+					delete = function(picker, item)
+						if item then
+							table.remove(harpoon_files.items, item.idx)
+							if harpoon_files.save then
+								harpoon_files:save()
+							end
+						end
+						picker:close()
+						toggle_snacks_picker(harpoon_files)
+					end,
+				},
+				win = {
+					input = {
+						keys = {
+							["dd"] = { "delete", mode = { "n" } },
+							["<C-d>"] = { "delete", mode = { "n", "i" } },
+						},
+					},
+				},
 			})
 		end
 
